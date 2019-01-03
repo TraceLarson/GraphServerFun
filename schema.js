55bab6a0cc7531d4fc64ba4f79a2a7af326ac656
Trace Larson <trace.larson4917@gmail.com>
2019-01-02 21:06:22 -0600

    created schema for searching hardcoded customer objects, as well as returning all customer objects

--- schema.js
+++ schema.js
@@ -7,6 +7,50 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
-// Root Query
+// Hardcoded data
+const customers = [
+  { id: "1", name: "Johnny", email: "john@gmail.com", age: 35 },
+  { id: "2", name: "Eddie", email: "eddie@gmail.com", age: 35 },
+  { id: "3", name: "George", email: "george@gmail.com", age: 35 }
+];
 
-module.exports = new GraphQLSchema({});
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
