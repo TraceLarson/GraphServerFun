7a462a2948a0a8142ee481404613f5799037b4c2
Trace Larson <trace.larson4917@gmail.com>
2019-01-02 20:46:18 -0600

    setup express server, install graphQL dependencies, created schema file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema");
+const port = 4000;
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
